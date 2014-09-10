e8b59a79cad73efda5ba8dc21832d792642e5c21
Benoit Prioux <benoit.prioux@gmail.com>
2014-09-10 23:19:45 +0200

    fix generate slide

--- generate-slides.sh
+++ generate-slides.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn clean package
+mvn clean package -Pslide
 
  ./deck2pdf/bin/deck2pdf ./target/generated-slides/asciidoctor-quickie.html ./target/generated-slides/asciidoctor-quickie.pdf
