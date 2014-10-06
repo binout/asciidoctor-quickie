fac65e351e14c288f7005f8b174e2416ae4f3271
Benoit Prioux <benoit.prioux@gmail.com>
2014-10-06 21:48:40 +0200

    update slide for bdx.io

--- generate-slides.sh
+++ generate-slides.sh
@@ -2,4 +2,7 @@
 
 mvn clean package -Pslide
 
- ./deck2pdf/bin/deck2pdf ./target/generated-slides/asciidoctor-quickie.html ./target/generated-slides/asciidoctor-quickie.pdf
+./deck2pdf/bin/deck2pdf ./target/generated-slides/asciidoctor-quickie.html ./target/generated-slides/asciidoctor-quickie.pdf
+./deck2pdf/bin/deck2pdf ./target/generated-slides/asciidoctor-bdx-io.html ./target/generated-slides/asciidoctor-bdx-io.pdf
+
+mvn install -Pgithub
